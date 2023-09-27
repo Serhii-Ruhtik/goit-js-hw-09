3838701ab6c5fbebd566ffc093858ed3ff5fb592
Serhii-Ruhtik <105732843+Serhii-Ruhtik@users.noreply.github.com>
2023-09-27 18:33:14 +0000

    Deploying to gh-pages from @ Serhii-Ruhtik/goit-js-hw-09@ef888ac72ccaf9989553fc6fba73552de43e0012 🚀

---
+++ 03-promises.41c2146c.js
@@ -0,0 +1,2 @@
+new Promise((function(n,o){var e=Math.random()>.5;setTimeout((function(){e&&n("YES"),o("NO")}),2e3)})).then((function(n){return console.log(n),5})).then((function(n){console.log(n)})).then((function(n){console.log(n)}));
+//# sourceMappingURL=03-promises.41c2146c.js.map
--- 03-promises.d7c148c9.js
+++
@@ -1,2 +0,0 @@
-console.log(10),console.log(10),console.log(10),console.log(10);
-//# sourceMappingURL=03-promises.d7c148c9.js.map
