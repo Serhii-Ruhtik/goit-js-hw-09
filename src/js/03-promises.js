ae15210b5874cca37425a78e67beca95a77f2b98
Serhii-Ruhtik <fopgavrilyk1@gmail.com>
2023-10-03 22:39:07 +0300

    03.10

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,91 +1,89 @@
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
+const refsForm = document.querySelector(".form")
+  // console.log(refsForm);
+
+  refsForm.addEventListener('submit', submitInPromiseGenerator)
+
+  function submitInPromiseGenerator(params) {
+    console.log(10);
+  };
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    // Fulfill
+  } else {
+    // Reject
+  }
+}
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+console.log(10);
 
 // !---------------------------------------
-// const promise = new Promise((resolve, reject) => {
-//   const canFulfill = Math.random() > 0.5;
 
-//   setTimeout(() => {
-//     if (canFulfill) {
-//       resolve('YES');
-//     }
-//     reject('NO');
-//   }, 2000);
-// });
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-// function onFulfilled(result) {
-//   console.log('onFulfilled -> onFulfilled');
-//   console.log(`✅ ${result}`);
+
+
+// const formField = document.querySelector('.form');
+// let timerId = null;
+// // let counter = 0;
+
+// formField.addEventListener('submit', submitHandler);
+
+// function submitHandler(event) {
+//   event.preventDefault();
+//   const {
+//     elements: { delay, step, amount },
+//   } = event.currentTarget;
+
+//   let del = Number(delay.value);
+
+//   for (let i = 1; i <= amount.value; i += 1) {
+//     createPromise(i, del)
+//       .then(({ position, delay }) => {
+//         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//       })
+//       .catch(({ position, delay }) => {
+//         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+//       });
+
+//     del += Number(step.value);
+//   }
+
+//   // while (counter < amount.value) {
+//   //   counter++;
+
+//   //   createPromise(counter, del)
+//   //     .then(({ position, delay }) => {
+//   //       Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   //     })
+//   //     .catch(({ position, delay }) => {
+//   //       Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+//   //     });
+
+//   //   del += Number(step.value);
+//   // }
+//   // counter = 0;
+//   event.currentTarget.reset();
 // }
 
-// function onRejected(error) {
-//   console.log('onRejected -> onRejected');
-//   console.log(`❌ ${error}`);
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+
+//   return new Promise((resolve, reject) => {
+//     timerId = setTimeout(() => {
+//       if (shouldResolve) {
+//         resolve({ position, delay });
+//       } else {
+//         reject({ position, delay });
+//       }
+//     }, delay);
+//   });
 // }
-
-// /**
-//  * ланцюжки промісів
-//  */
-
-// promise
-//   .then(onFulfilled, onRejected)
-//   .then(x => {
-//     console.log(x);
-
-//     return 10;
-//   })
-//   .then(y => console.log(y))
-//   .then(a => console.log(a))
-//   .catch(error => console.log(error))
-//   .finally(() => console.log('Я буду виконуватися завжди'));
-
-/*
- * Промисификация:
- * - Поблема доступа к результату промиса с колбеком
- * - Функция которая возвращает промис
- */
-
-// const makeOrder = (dish) => {
-//   const DELAY = 1000;
-
-//   const promise = new Promise((resolve, reject) =>
-//   {
-//     const passed = Math.random() > 0.5;
-//   setTimeout(() => {
-//     if (passed) {
-//       resolve('Your food');
-//     } else {
-//       reject('your food is not ready');
-//     }
-//   }, DELAY);
-//   })
-//   return promise
-// };
-
-// makeOrder('пиріжок').then(onMakeOrderSuccess).catch(onMakeOrderError) ;
-
-// function onMakeOrderSuccess(result) {
-//   console.log('onMakeOrderSuccess');
-//   console.log(result);
-// }
-// function onMakeOrderError(error) {
-//   console.log('onMakeOrderError');
-//   console.log(error);
-// }
-// console.log(10);
-// console.log(20);
-// console.log(30);
-// console.log(40);
-// console.log(50);
-
-const promise = new Promise((resolve, reject) => {
-  reject("Whoops")
-})
-promise.then(value => console.log(value));
-console.log(10);
