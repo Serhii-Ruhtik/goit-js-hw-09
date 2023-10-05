750b3bd462dee73b3a8daed56c502437054377be
Serhii-Ruhtik <fopgavrilyk1@gmail.com>
2023-10-05 14:30:55 +0300

    finish task 03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,11 +1,11 @@
-// import { Notify } from 'notiflix';
+import { Notify } from 'notiflix';
 
 
 
 const refsForm = document.querySelector('.form');
 // console.log(refsForm);
 
-let timerId = null; 
+// let timerId = null; 
 
 refsForm.addEventListener('submit', submitInPromiseGenerator);
 
@@ -13,7 +13,7 @@ function createPromise(position, delay) {
  
 return new Promise((resolve, reject) => {
   const shouldResolve = Math.random() > 0.3;
-  timerId = setTimeout(() => {
+  setTimeout(() => {
     if (shouldResolve) {
       resolve({ position, delay });
     } else {
@@ -30,15 +30,15 @@ function submitInPromiseGenerator(params) {
   const { delay, step, amount } = params.currentTarget.elements;
   let del = Number(delay.value);
 
-  for (let i = 0; i <= amount.value; i+= 1) {
+  for (let i = 0; i < amount.value; i+= 1) {
     
 createPromise(i, del)
   .then(({ position, delay }) => {
-    // Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
   })
   .catch(({ position, delay }) => {
-    // Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
   });
   del += Number(step.value);
